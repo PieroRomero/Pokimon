177d1602442cde8db4ae1ea0f19957b289b801cb
PieroRomero <elpierito3005@gmail.com>
2019-06-05 12:03:20 -0500

    encapsulamiento

--- PokimonApp/src/entidades/Pokimon.java
+++ PokimonApp/src/entidades/Pokimon.java
@@ -2,19 +2,92 @@ package entidades;
 
 public class Pokimon
 {
-    String especie;
-    String nombre;
-    String tipo;
-    int vidaMax;
-    int salud;
-    int nivel;
-    int atk;
+    private String especie;
+    private String nombre;
+    private String tipo;
+    private int vidaMax;
+    private int salud;
+    private int nivel;
+    private int atk;
     
 
     public Pokimon(String species, String type) 
     {
         especie = species;
         tipo = type;
+        nivel = 1;
+        vidaMax = 40 + nivel*5;
+        salud = vidaMax;
+    }
+
+    public String getEspecie() 
+    {
+        return especie;
+    }
+
+    public void setEspecie(String especie) 
+    {
+        this.especie = especie;
+    }
+
+    public String getNombre() 
+    {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) 
+    {
+        this.nombre = nombre;
+    }
+
+    public String getTipo() 
+    {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) 
+    {
+        this.tipo = tipo;
+    }
+
+    public int getVidaMax() 
+    {
+        return vidaMax;
+    }
+
+    public void setVidaMax(int vidaMax)
+    {
+        this.vidaMax = vidaMax;
+    }
+
+    public int getSalud() 
+    {
+        return salud;
+    }
+
+    public void setSalud(int salud) 
+    {
+        this.salud = salud;
+    }
+
+    public int getNivel()
+    {
+        return nivel;
+    }
+
+    public void setNivel(int nivel)
+    {
+        this.nivel = nivel;
+    }
+
+    public int getAtk()
+    {
+        return atk;
+    }
+
+    public void setAtk(int atk)
+    {
+        this.atk = atk;
     }
     
     
